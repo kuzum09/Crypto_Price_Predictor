de74cfb59b10853050bd8873568a8856c7219dd5
Kusum Patel <126418779+kuzum09@users.noreply.github.com>
2023-06-14 23:02:27 +0530

    Add files via upload
    Signed-off-by: Kusum Patel <126418779+kuzum09@users.noreply.github.com>

---
+++ Crypto_Price_Predictor.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+root=Tk()
+bgimage=PhotoImage(file="C:\Users\Asus\Desktop\IIIT NR\2nd Sem\AIML Files\final project\bg.jpeg")
+pk=Label(root,i=bgimage)
+pk.pack()
+# root.title('Crypto Price Predictor')
+# l=tk.Label(text='Crypto Price Predictor ',font=('Times New Roman', 50, 'bold'))
+# l.pack()
+root.mainloop()
